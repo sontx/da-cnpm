f4ab28e01dbdfecedfd983ca3fb76d8970f58889
trongvn13 <vonhutrong@gmail.com>
2016-10-23 17:13:51 +0700

    ! change the way servlet do work

--- CinemaProject/src/com/dutproject/cinemaproject/controller/LoginActionServlet.java
+++ CinemaProject/src/com/dutproject/cinemaproject/controller/LoginActionServlet.java
@@ -1,9 +1,15 @@
 package com.dutproject.cinemaproject.controller;
 
 import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.dutproject.cinemaproject.model.bean.Account;
+import com.dutproject.cinemaproject.model.bean.Account.Permission;
 import com.dutproject.cinemaproject.model.bo.AccountBO;
 
 /**
@@ -18,29 +24,41 @@ public class LoginActionServlet extends BaseServlet {
     }
 
 	@Override
-	protected void doWork() throws IOException {
+	protected void doWork(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
 		Account account = new Account(username, password);
 		
 		AccountBO accountBO = new AccountBO();
-		switch (accountBO.isValidAccount(account)) {
+		Account.Permission permission = accountBO.isValidAccount(account);
+		if (permission != Account.Permission.NO_PERMISSION) {
+			saveUsernameToSession(request.getSession(), account.getUsername());
+			savePermissionToSessioin(request.getSession(), permission);
+		}
+		switch (permission) {
 		case FILM_MANAGER:
-			this.saveUsernameToSession(account);
-			this.sendRedirect("jsp/filmManager.jsp");
+			response.sendRedirect("jsp/filmManagement.jsp");
 			break;
 		case ROOM_MANAGER:
 			break;
+		case SCHEDULE_MANAGER:
+			request.getRequestDispatcher("/ScheduleManagement").forward(request, response);
+			break;
 		case NO_PERMISSION:
-			this.sendRedirect("jsp/loginForm.jsp");
+			response.sendRedirect("jsp/loginForm.jsp");
 			break;
 		default:
 			break;
 		}
 	}
-	
-	private void saveUsernameToSession(Account account) {
-		this.setAttribute("username", account.getUsername());
+
+	private void savePermissionToSessioin(HttpSession session, Permission permission) {
+		session.setAttribute("permission", permission);
+	}
+
+	private void saveUsernameToSession(HttpSession session, String username) {
+		session.setAttribute("username", username);
 	}
 
 }
--- CinemaProject/src/com/dutproject/cinemaproject/controller/LogoutServlet.java
+++ CinemaProject/src/com/dutproject/cinemaproject/controller/LogoutServlet.java
@@ -1,7 +1,11 @@
 package com.dutproject.cinemaproject.controller;
 
 import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Servlet implementation class LogoutServlet
@@ -15,9 +19,10 @@ public class LogoutServlet extends BaseServlet {
     }
 
 	@Override
-	protected void doWork() throws IOException {
-		this.request.getSession().invalidate();
-		this.sendRedirect("jsp/loginForm.jsp");
+	protected void doWork(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.getSession().invalidate();
+		response.sendRedirect("jsp/loginForm.jsp");
 	}
 
 }
