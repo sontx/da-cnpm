db35aa627bc146c32916c034ec51d90e80220aac
sontx <xuanson33bk@gmail.com>
2016-10-24 14:15:12 +0700

    * unresolve doWork method

--- CinemaProject/src/com/dutproject/cinemaproject/controller/film/FilmsListSeverlet.java
+++ CinemaProject/src/com/dutproject/cinemaproject/controller/film/FilmsListSeverlet.java
@@ -8,6 +8,8 @@ import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import com.dutproject.cinemaproject.controller.BaseServlet;
 import com.dutproject.cinemaproject.model.bean.Film;
@@ -16,26 +18,12 @@ import com.dutproject.cinemaproject.model.bo.FilmBo;
 /**
  * @author HoVanLy
  *
- * Oct 23, 2016
+ *         Oct 23, 2016
  */
 @WebServlet(name = "FilmsList", urlPatterns = { "/film/FilmList" })
 public class FilmsListSeverlet extends BaseServlet {
 	private FilmBo mFilmBo = FilmBo.getInstance();
 	private static final long serialVersionUID = 1L;
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.dutproject.cinemaproject.controller.BaseServlet#doWork()
-	 */
-	@Override
-	protected void doWork() throws IOException, ServletException {
-		String sPageNumber = request.getParameter("page");
-		int pageNumber = tryParseInt(sPageNumber, 1) - 1;
-		List<Film> films = mFilmBo.getFilms(pageNumber * 50, 50);
-		setAttribute("films", films);
-		request.getRequestDispatcher("/jsp/film/FilmList.jsp").forward(request, response);
-	}
 
 	private int tryParseInt(String st, int defaultValue) {
 		try {
@@ -43,5 +31,15 @@ public class FilmsListSeverlet extends BaseServlet {
 		} catch (NumberFormatException ignored) {
 			return defaultValue;
 		}
+	}
+
+	@Override
+	protected void doWork(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String sPageNumber = request.getParameter("page");
+		int pageNumber = tryParseInt(sPageNumber, 1) - 1;
+		List<Film> films = mFilmBo.getFilms(pageNumber * 50, 50);
+		request.setAttribute("films", films);
+		request.getRequestDispatcher("/jsp/film/FilmList.jsp").forward(request, response);
 	}
 }
