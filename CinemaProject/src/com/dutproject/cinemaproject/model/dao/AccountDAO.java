108a28329490f2e0128d9512b6d36862714f46f3
trongvn13 <vonhutrong@gmail.com>
2016-11-11 00:16:42 +0700

    + apply bootstrap for film ui

--- CinemaProject/src/com/dutproject/cinemaproject/model/dao/AccountDAO.java
+++ CinemaProject/src/com/dutproject/cinemaproject/model/dao/AccountDAO.java
@@ -11,6 +11,8 @@ public class AccountDAO {
 			return Account.SCHEDULE_MANAGER;
 		} else if ("admin3".equals(account.getUsername())) {
 			return Account.ROOM_MANAGER;
+		} else if ("adminfilm".equals(account.getUsername())) {
+			return Account.FILM_MANAGER;
 		}
 		return Account.NO_PERMISSION;
 	}
