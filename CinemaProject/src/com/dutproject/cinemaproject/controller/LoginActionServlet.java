a875e11ca678dd1590f763aaf300a1fc9c13dbc9
sontx <xuanson33bk@gmail.com>
2016-11-21 01:54:53 +0700

    * wrong home link

--- CinemaProject/src/com/dutproject/cinemaproject/controller/LoginActionServlet.java
+++ CinemaProject/src/com/dutproject/cinemaproject/controller/LoginActionServlet.java
@@ -36,21 +36,27 @@ public class LoginActionServlet extends BaseServlet {
 		}
 
 		String site = "Login";
+		String home = "Login";
 		switch (permission) {
 		case Account.ACCOUNT_MANAGER:
 			site = "Admin/StaffsList";
+			home = "StaffsList";
 			break;
 		case Account.FILM_MANAGER:
 			site = "film/FilmList";
+			home = "FilmList";
 			break;
 		case Account.ROOM_MANAGER:
 			site = "Room/RoomsList";
+			home = "RoomsList";
 			break;
 		case Account.SCHEDULE_MANAGER:
 			site = "ScheduleManagement";
+			home = "ScheduleManagement";
 			break;
 		case Account.TICKET_MANAGER:
 			site = "MovieListServlet";
+			home = "MovieListServlet";
 			break;
 		case Account.NO_PERMISSION:
 			break;
@@ -58,7 +64,7 @@ public class LoginActionServlet extends BaseServlet {
 			break;
 		}
 		if (permission != Account.NO_PERMISSION)
-			request.getSession().setAttribute("home", site);
+			request.getSession().setAttribute("home", home);
 		response.sendRedirect(site);
 	}
 
